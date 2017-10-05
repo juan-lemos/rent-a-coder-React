8a5958a5156a9f492aeac13254bc1731f97a4e38
juanpale <juanpale@users.noreply.github.com>
2017-10-05 20:12:57 -0300

    generate profile page

---
+++ app/containers/ProfilePage/index.js
@@ -0,0 +1,54 @@
+/**
+ *
+ * ProfilePage
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import injectSaga from 'utils/injectSaga';
+import injectReducer from 'utils/injectReducer';
+import makeSelectProfilePage from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+import messages from './messages';
+
+export class ProfilePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+        <FormattedMessage {...messages.header} />
+      </div>
+    );
+  }
+}
+
+ProfilePage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  profilepage: makeSelectProfilePage(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(mapStateToProps, mapDispatchToProps);
+
+const withReducer = injectReducer({ key: 'profilePage', reducer });
+const withSaga = injectSaga({ key: 'profilePage', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(ProfilePage);

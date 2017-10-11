e00a62edf98011bfd0e253de307e1509ec16f395
juanpale <juanpale@users.noreply.github.com>
2017-10-11 09:34:28 -0300

    log in first components

--- app/containers/LoginPage/index.js
+++ app/containers/LoginPage/index.js
@@ -7,22 +7,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { FormattedMessage } from 'react-intl';
+// import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 
+import { Button, Form, FormGroup, Col, FormControl, Checkbox } from 'react-bootstrap';
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
 import makeSelectLoginPage from './selectors';
 import reducer from './reducer';
 import saga from './saga';
-import messages from './messages';
+// import messages from './messages';
 
 export class LoginPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  ControlLabel = 'hola';
   render() {
     return (
       <div>
-        <FormattedMessage {...messages.header} />
+        <Form horizontal>
+          <FormGroup controlId="formHorizontalEmail">
+            <Col componentClass={this.ControlLabel} sm={2}>
+              Email
+          </Col>
+            <Col sm={10}>
+              <FormControl type="email" placeholder="Email" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup controlId="formHorizontalPassword">
+            <Col componentClass={this.ControlLabel} sm={2}>
+              Password
+          </Col>
+            <Col sm={10}>
+              <FormControl type="password" placeholder="Password" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Checkbox>Remember me</Checkbox>
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Button type="submit">
+                Sign in
+            </Button>
+            </Col>
+          </FormGroup>
+        </Form>
       </div>
     );
   }

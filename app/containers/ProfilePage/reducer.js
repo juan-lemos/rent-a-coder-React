035061213e0458fe06c6046696f93c2f34afee79
Juan Lemos <juanpale@users.noreply.github.com>
2017-11-05 13:03:25 -0300

    change profile sucess action
    only request for action content not body

--- app/containers/ProfilePage/reducer.js
+++ app/containers/ProfilePage/reducer.js
@@ -15,7 +15,7 @@ function registerPageReducer(state = initialState, action) {
       return state;
     case PROFILE_SUCCESS:
       return state
-        .set('userData', action.content.body);
+        .set('userData', action.content);
     default:
       return state;
   }

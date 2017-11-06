9a35233adeede3099c29f92bd0b9f523faf85dea
juanpale <juanpale@users.noreply.github.com>
2017-11-06 02:17:40 -0300

    listpanels now is a react.purecomponent

--- app/components/HomeComponents/ListPanels/index.js
+++ app/components/HomeComponents/ListPanels/index.js
@@ -14,36 +14,29 @@ const header = (name, deadline, isOpen) => (
   </span >
 );
 
-function ListPanels({ projects }) {
-  const projectsPanels = projects.map((project, i) => {
-    const id = `panelProject${i}`;
+
+class ListPanels extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    const projectsPanels = this.props.projects.map((project, i) => {
+      const id = `panelProject${i}`;
+      return (
+        <Panel key={id} header={header(project.name, project.deadline, false)} eventKey={id}>
+          <Body
+            tags={['sdfs', 'dfsdf', 'dfsdf']}
+            description={project.description}
+            employerName={project.owner.name}
+            starts={project.owner.owner_score}
+          />
+        </Panel>
+      );
+    }
+    );
     return (
-      <Panel key={id} header={header(project.name, project.deadline, false)} eventKey={id}>
-        <Body
-          tags={['sdfs', 'dfsdf', 'dfsdf']}
-          description={project.description}
-          employerName={project.owner.name}
-          starts={project.owner.owner_score}
-        />
-      </Panel>
+      <Accordion >
+        {projectsPanels}
+      </Accordion>
     );
   }
-  );
-
-  return (
-    <Accordion >
-      {projectsPanels}
-      {/* <Panel header="dfsdfs" eventKey={2}>
-        <div style={{ height: '30px' }} />
-      </Panel>
-      <Panel header="Collapsible Group Item #2" eventKey={3}>
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably havent heard of them accusamus labore sustainable VHS.
-          </Panel>
-      <Panel header="Collapsible Group Item #3" eventKey={4}>
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably havent heard of them accusamus labore sustainable VHS.
-          </Panel> */}
-    </Accordion>
-  );
 }
 
 ListPanels.propTypes = {

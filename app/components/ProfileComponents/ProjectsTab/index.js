1c870d1ebadb264e53061a76864d5aa9a9986cfe
Mauricio Coniglio <mauricio.coniglio@moove-it.com>
2017-11-09 20:26:10 -0300

    Add extra info to each project on profile page

--- app/components/ProfileComponents/ProjectsTab/index.js
+++ app/components/ProfileComponents/ProjectsTab/index.js
@@ -1,16 +1,56 @@
 import React from 'react';
-import { Tab, Accordion, Panel } from 'react-bootstrap';
+import { Tab, Accordion, Panel, Label } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import './index.css';
 
 function ProjectsTab({ eventKey, title, projects }) {
+  const stateClasses = {
+    open: 'success',
+    in_progress: 'primary',
+    danger: 'finished',
+  };
+
   let projectElements;
   if (projects) {
-    projectElements = projects.map((project) => (
-      <Panel header={project.name} eventKey={project.id} key={project.id}>
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably havent heard of them accusamus labore sustainable VHS.
-      </Panel>
-    ));
+    projectElements = projects.map((project) => {
+      const projectHeader = (
+        <div className="project-header">
+          <h5>{project.name}</h5>
+          <Label bsStyle={stateClasses[project.state]}>{project.state}</Label>
+        </div>
+      );
+
+      let projectTags;
+      if (project.technologies.length !== 0) {
+        projectTags = project.technologies.map((element, i) => {
+          const keyTag = `tag${i}`;
+          return (
+            <span key={keyTag} style={{ marginRight: '5px' }}>
+              <Label>{element.name}</Label>
+            </span>
+          );
+        });
+      } else {
+        projectTags = '';
+      }
+
+      return (
+        <Panel header={projectHeader} eventKey={project.id} key={project.id}>
+          <p className="project-body-section">
+            <strong className="project-body-title">Descripción</strong>
+            {project.description}
+          </p>
+          <p className="project-body-section">
+            <strong className="project-body-title">Tecnologías</strong>
+            {projectTags}
+          </p>
+          <p className="project-body-section">
+            <strong className="project-body-title">Deadline</strong>
+            {project.deadline}
+          </p>
+        </Panel>
+      );
+    });
   } else {
     projectElements = '';
   }

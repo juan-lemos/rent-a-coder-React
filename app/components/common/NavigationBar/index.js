7d59100704bdbe8f4b297fb32bab2d47913e06ef
juanpale <juanpale@users.noreply.github.com>
2017-11-16 01:51:14 -0300

    correction logout;

--- app/components/common/NavigationBar/index.js
+++ app/components/common/NavigationBar/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 // import styled from 'styled-components';
-
+import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import {
@@ -12,64 +12,72 @@ import {
 import messages from './messages';
 
 
-function chooseMessage() {
-  if (getSessionToken() == null || getSessionToken() === 'null') {
-    return <FormattedMessage {...messages.login} />;
-  }
-  return (
-    <div // eslint-disable-line
-      onClick={() => resetLogginVariables()}
-    >
+export class NavigationBar extends React.PureComponent {
+
+  chooseMessage() {
+    if (getSessionToken() == null || getSessionToken() === 'null') {
+      return <FormattedMessage {...messages.login} />;
+    }
+    return (
       <FormattedMessage {...messages.logout} />
-    </div>
-  );
+    );
+  }
+
+  resetVariablesLoginAndMoveLogin() {
+    resetLogginVariables();
+    this.props.history.push('/profile');
+  }
+
+
+  render() {
+    const path = this.props.location.pathname;
+    const navbarInstance = (
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">
+              <FormattedMessage {...messages.pageName} />
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <LinkContainer to="/publishProject">
+              <NavItem eventKey={1}>
+                <FormattedMessage {...messages.publish} />
+              </NavItem>
+            </LinkContainer>
+          </Nav>
+          <Nav pullRight>
+            <NavItem
+              eventKey={2}
+              active={path === '/login'}
+              onClick={() => this.resetVariablesLoginAndMoveLogin()}
+            >
+              {this.chooseMessage()}
+            </NavItem>
+            <LinkContainer to="/profile">
+              <NavItem eventKey={3}>
+                <FormattedMessage {...messages.profile} />
+              </NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar >
+    );
+
+    return (
+
+      <div>
+        {navbarInstance}
+      </div>
+    );
+  }
 }
-
-function NavigationBar() {
-  const navbarInstance = (
-    <Navbar inverse collapseOnSelect>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <Link to="/">
-            <FormattedMessage {...messages.pageName} />
-          </Link>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
-      <Navbar.Collapse>
-        <Nav>
-          <LinkContainer to="/publishProject">
-            <NavItem eventKey={1}>
-              <FormattedMessage {...messages.publish} />
-            </NavItem>
-          </LinkContainer>
-        </Nav>
-        <Nav pullRight>
-          <LinkContainer to="/login">
-            <NavItem eventKey={2}>
-              {chooseMessage()}
-            </NavItem>
-          </LinkContainer>
-          <LinkContainer to="/profile">
-            <NavItem eventKey={3}>
-              <FormattedMessage {...messages.profile} />
-            </NavItem>
-          </LinkContainer>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
-
-  return (
-
-    <div>
-      {navbarInstance}
-    </div>
-  );
-}
-
 NavigationBar.propTypes = {
-
+  history: PropTypes.object,
+  location: PropTypes.object,
 };
 
-export default NavigationBar;
+export default withRouter(NavigationBar);
